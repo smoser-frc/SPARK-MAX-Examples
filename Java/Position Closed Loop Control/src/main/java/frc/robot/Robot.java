8d2bae01f4e5a4d14720fb436d24407324a24d77
Scott Moser <smoser@brickies.net>
2022-03-17 16:58:44 +0000

    It builds.
    Still TODO:
     * fix update the values in the motors.
     * I'm not sure how to get the motors shuffleboard entry after i created it.
    
    Probalby best thing to do is define a class MyMotor that has
     * motor
     * name
     * encoder
     * pidcontroller
     * networktable targetRotations
     * networktable currentRotations
    
    And then just keep an arrayList of those rathe rthan all the array
    lists.

--- Java/Position Closed Loop Control/src/main/java/frc/robot/Robot.java
+++ Java/Position Closed Loop Control/src/main/java/frc/robot/Robot.java
@@ -7,8 +7,17 @@
 
 package frc.robot;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
 
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
@@ -16,102 +25,209 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Robot extends TimedRobot {
-  private static final int deviceID = 1;
-  private CANSparkMax m_motor;
-  private SparkMaxPIDController m_pidController;
-  private RelativeEncoder m_encoder;
-  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
+  private static final int lFrontID = 1;
+  private static final int lRearID = 2;
+  private static final int rFrontID = 4;
+  private static final int rRearID = 3;
+  private CANSparkMax lFrontMotor, lRearMotor, rFrontMotor, rRearMotor;
+  private ShuffleboardTab shuffTab = Shuffleboard.getTab("SparkMax");
+  private NetworkTableEntry ntEntryP, ntEntryI, ntEntryD, ntEntryIz,
+          ntEntryFF, ntEntryMaxOut, ntEntryMinOut,
+          driveGo, driveRotations;
+  private boolean lastGo = false;
+  private double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
+  private ArrayList<CANSparkMax> motors = new ArrayList<CANSparkMax>();
+  private ArrayList<SparkMaxPIDController> pidControllers = new ArrayList<SparkMaxPIDController>();
+  private ArrayList<String> names = new ArrayList<String>();
 
-  @Override
-  public void robotInit() {
+  private CANSparkMax motorInit(String name, int canID) {
     // initialize motor
-    m_motor = new CANSparkMax(deviceID, MotorType.kBrushless);
+    CANSparkMax m = new CANSparkMax(canID, MotorType.kBrushless);
 
     /**
      * The restoreFactoryDefaults method can be used to reset the configuration parameters
      * in the SPARK MAX to their factory default state. If no argument is passed, these
      * parameters will not persist between power cycles
      */
-    m_motor.restoreFactoryDefaults();
+    m.restoreFactoryDefaults();
 
     /**
      * In order to use PID functionality for a controller, a SparkMaxPIDController object
      * is constructed by calling the getPIDController() method on an existing
      * CANSparkMax object
      */
-    m_pidController = m_motor.getPIDController();
+    SparkMaxPIDController pidc = m.getPIDController();
 
     // Encoder object created to display position values
-    m_encoder = m_motor.getEncoder();
-
-    // PID coefficients
-    kP = 0.1; 
-    kI = 1e-4;
-    kD = 1; 
-    kIz = 0; 
-    kFF = 0; 
-    kMaxOutput = 1; 
-    kMinOutput = -1;
+    RelativeEncoder encoder = m.getEncoder();
 
     // set PID coefficients
-    m_pidController.setP(kP);
-    m_pidController.setI(kI);
-    m_pidController.setD(kD);
-    m_pidController.setIZone(kIz);
-    m_pidController.setFF(kFF);
-    m_pidController.setOutputRange(kMinOutput, kMaxOutput);
+    pidc.setP(kP);
+    pidc.setI(kI);
+    pidc.setD(kD);
+    pidc.setIZone(kIz);
+    pidc.setFF(kFF);
+    pidc.setOutputRange(kMinOutput, kMaxOutput);
 
-    // display PID coefficients on SmartDashboard
-    SmartDashboard.putNumber("P Gain", kP);
-    SmartDashboard.putNumber("I Gain", kI);
-    SmartDashboard.putNumber("D Gain", kD);
-    SmartDashboard.putNumber("I Zone", kIz);
-    SmartDashboard.putNumber("Feed Forward", kFF);
-    SmartDashboard.putNumber("Max Output", kMaxOutput);
-    SmartDashboard.putNumber("Min Output", kMinOutput);
-    SmartDashboard.putNumber("Set Rotations", 0);
+    // SmartDashboard.putNumber("Set Rotations", 0);
+    ShuffleboardLayout motorGroup = shuffTab.getLayout(name, BuiltInLayouts.kList);
+    motorGroup.add("Rotations", encoder.getPosition());
+    motorGroup.add("Target", encoder.getPosition());
+
+    return m;
+  }
+
+
+  @Override
+  public void robotInit() {
+    // PID coefficients
+    kP = 0.1;
+    kI = 1e-4;
+    kD = 1;
+    kIz = 0;
+    kFF = 0;
+    kMaxOutput = 1;
+    kMinOutput = -1;
+
+    ShuffleboardLayout pidGroup = shuffTab.getLayout("PID", BuiltInLayouts.kList);
+    ntEntryP = pidGroup.add("P Gain", kP).getEntry();
+    ntEntryI = pidGroup.add("I Gain", kI).getEntry();
+    ntEntryD = pidGroup.add("D Gain", kD).getEntry();
+    ntEntryIz = pidGroup.add("I Zone", kIz).getEntry();
+    ntEntryFF = pidGroup.add("FeedForward", kFF).getEntry();
+    ntEntryMaxOut = pidGroup.add("MaxOut", kMaxOutput).
+            withWidget(BuiltInWidgets.kNumberSlider).
+            withProperties(Map.of("min", 0, "max", 1)).getEntry();
+    ntEntryMinOut = pidGroup.add("MinOut", kMinOutput).
+            withWidget(BuiltInWidgets.kNumberSlider).
+            withProperties(Map.of("min", 0, "max", 1)).getEntry();
+
+
+    ShuffleboardLayout driveGroup = shuffTab.getLayout("Drive", BuiltInLayouts.kList);
+    driveGo = driveGroup.add("Go", false).getEntry();
+    driveRotations = driveGroup.add("Rotations", 0).getEntry();
+
+    lFrontMotor = motorInit("LFront", lFrontID);
+    lRearMotor = motorInit("LRear", lRearID);
+    rFrontMotor = motorInit("RFront", rFrontID);
+    rRearMotor = motorInit("RRear", rRearID);
+
+    motors.add(lFrontMotor); names.add("LFront");
+    motors.add(lRearMotor);  names.add("LRear");
+    motors.add(rFrontMotor); names.add("RFront");
+    motors.add(rRearMotor);  names.add("RRear");
+
+    for (int i=0; i<motors.size(); i++) {
+      pidControllers.add(motors.get(i).getPIDController());
+    }
+  }
+
+  private void updatePidControllers() {
+    // read PID coefficients from SmartDashboard
+    double xP = ntEntryP.getDouble(0.0);
+    double xI = ntEntryI.getDouble(0.0);
+    double xD = ntEntryD.getDouble(0.0);
+    double xIz = ntEntryIz.getDouble(0.0);
+    double xFF = ntEntryFF.getDouble(0.0);
+    double xMax = ntEntryMaxOut.getDouble(0.0);
+    double xMin = ntEntryMinOut.getDouble(0.0);
+    // double rotations = SmartDashboard.getNumber("Set Rotations", 0);
+
+    if (xP != kP) {
+        kP = xP;
+        for (int i=0; i<pidControllers.size(); i++) {
+            pidControllers.get(i).setP(xP);
+        }
+    }
+
+    if (xI != kI) {
+        kI = xI;
+        for (int i=0; i<pidControllers.size(); i++) {
+            pidControllers.get(i).setI(xI);
+        }
+    }
+
+    if (xD != kD) {
+        kD = xD;
+        for (int i=0; i<pidControllers.size(); i++) {
+            pidControllers.get(i).setD(xD);
+        }
+    }
+
+    if (xIz != kIz) {
+        kIz = xIz;
+        for (int i=0; i<pidControllers.size(); i++) {
+            pidControllers.get(i).setIZone(xIz);
+        }
+    }
+
+    if (xFF != kFF) {
+        kFF = xFF;
+        for (int i=0; i<pidControllers.size(); i++) {
+            pidControllers.get(i).setFF(xFF);
+        }
+    }
+
+    if ((xMax != kMaxOutput) || (xMin != kMaxOutput)) {
+        kMinOutput = xMax;
+        kMaxOutput = xMax;
+        for (int i=0; i<pidControllers.size(); i++) {
+            pidControllers.get(i).setOutputRange(xMin, xMax);
+        }
+    }
   }
 
   @Override
-  public void teleopPeriodic() {
-    // read PID coefficients from SmartDashboard
-    double p = SmartDashboard.getNumber("P Gain", 0);
-    double i = SmartDashboard.getNumber("I Gain", 0);
-    double d = SmartDashboard.getNumber("D Gain", 0);
-    double iz = SmartDashboard.getNumber("I Zone", 0);
-    double ff = SmartDashboard.getNumber("Feed Forward", 0);
-    double max = SmartDashboard.getNumber("Max Output", 0);
-    double min = SmartDashboard.getNumber("Min Output", 0);
-    double rotations = SmartDashboard.getNumber("Set Rotations", 0);
-
-    // if PID coefficients on SmartDashboard have changed, write new values to controller
-    if((p != kP)) { m_pidController.setP(p); kP = p; }
-    if((i != kI)) { m_pidController.setI(i); kI = i; }
-    if((d != kD)) { m_pidController.setD(d); kD = d; }
-    if((iz != kIz)) { m_pidController.setIZone(iz); kIz = iz; }
-    if((ff != kFF)) { m_pidController.setFF(ff); kFF = ff; }
-    if((max != kMaxOutput) || (min != kMinOutput)) { 
-      m_pidController.setOutputRange(min, max); 
-      kMinOutput = min; kMaxOutput = max; 
-    }
-
+  public void autonomousPeriodic() {
     /**
-     * PIDController objects are commanded to a set point using the 
+     * PIDController objects are commanded to a set point using the
      * SetReference() method.
-     * 
+     *
      * The first parameter is the value of the set point, whose units vary
      * depending on the control type set in the second parameter.
-     * 
-     * The second parameter is the control type can be set to one of four 
+     *
+     * The second parameter is the control type can be set to one of four
      * parameters:
      *  com.revrobotics.CANSparkMax.ControlType.kDutyCycle
      *  com.revrobotics.CANSparkMax.ControlType.kPosition
      *  com.revrobotics.CANSparkMax.ControlType.kVelocity
      *  com.revrobotics.CANSparkMax.ControlType.kVoltage
      */
-    m_pidController.setReference(rotations, CANSparkMax.ControlType.kPosition);
-    
-    SmartDashboard.putNumber("SetPoint", rotations);
-    SmartDashboard.putNumber("ProcessVariable", m_encoder.getPosition());
+    boolean curGo = driveGo.getBoolean(false);
+    if (curGo == lastGo) {
+        // no change. We start or stop on Go toggle;
+        RelativeEncoder enc;
+        for (int i=0; i<motors.size(); i++) {
+            enc = motors.get(i).getEncoder();
+	    // TODO: update the motors current shuffleboard value.
+        }
+    } else {
+        double rotations = driveRotations.getDouble(0.0f);
+        updatePidControllers();
+        RelativeEncoder enc;
+        if (curGo) {
+            for(int i=0; i<pidControllers.size(); i++) {
+                enc = motors.get(i).getEncoder();
+                double target = enc.getPosition() + rotations;
+                pidControllers.get(i).setReference(target, CANSparkMax.ControlType.kPosition);
+                // TODO: update the motor's target shuffleboard Value.
+            }
+        } else {
+            for(int i=0; i<pidControllers.size(); i++) {
+                enc = motors.get(i).getEncoder();
+                pidControllers.get(i).setReference(enc.getPosition(), CANSparkMax.ControlType.kPosition);
+                // TODO: update the motor's target shuffleboard Value.
+            }
+        }
+        lastGo = curGo;
+        // m_pidController.setReference(rotations, CANSparkMax.ControlType.kPosition);
+        // SmartDashboard.putNumber("SetPoint", rotations);
+        // SmartDashboard.putNumber("ProcessVariable", m_encoder.getPosition());
+    }
+
+  }
+
+  @Override
+  public void teleopPeriodic() {
   }
 }
